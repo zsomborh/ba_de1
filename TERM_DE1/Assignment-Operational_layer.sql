beee6decc8a838d31b8a84b8bd4738ad6968914a
Zsombor Hegedus <zsomborhegedus@gmail.com>
2020-11-13 00:52:40 +0100

    chg operational layer to add database linkage + add pngs

--- TERM_DE1/Assignment-Operational_layer.sql
+++ TERM_DE1/Assignment-Operational_layer.sql
@@ -16,7 +16,8 @@ CREATE TABLE country
 (
 country_id INTEGER NOT NULL,
 country_name VARCHAR(255),
-PRIMARY KEY(country_id));
+PRIMARY KEY(country_id)
+);
 
 LOAD DATA INFILE 'c:/ProgramData/MySQL/MySQL Server 8.0/Uploads/country.csv' 
 INTO TABLE country
@@ -33,7 +34,9 @@ CREATE TABLE league
 league_id INTEGER NOT NULL,
 country_id integer,
 league_name varchar(255),
-PRIMARY KEY(league_id));
+PRIMARY KEY(league_id),
+key country_id  (country_id),
+CONSTRAINT league_ibfk_1 FOREIGN KEY (country_id) REFERENCES country (country_id));
 
 LOAD DATA INFILE 'c:/ProgramData/MySQL/MySQL Server 8.0/Uploads/league.csv' 
 INTO TABLE league
@@ -42,6 +45,43 @@ ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n' 
 ignore 1 rows
 ;
+
+drop table if exists teams;
+
+CREATE TABLE teams 
+(
+id INTEGER NOT NULL,
+team_api_id integer not null,
+team_fifa_api_id  integer,
+team_long_name varchar (255), 
+team_short_name varchar(3), 
+PRIMARY KEY(team_api_id),
+key team_api_id (team_api_id));
+
+LOAD DATA INFILE 'c:/ProgramData/MySQL/MySQL Server 8.0/Uploads/team.csv' 
+INTO TABLE teams
+CHARACTER SET CP1250
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\r\n' 
+ignore 1 rows
+(id, team_api_id, @team_fifa_api_id, team_long_name, team_short_name)
+SET
+team_fifa_api_id = nullif(@team_fifa_api_id, 'NA')
+;
+
+DROP TABLE IF EXISTS players;
+
+CREATE TABLE players (
+id int not null,
+player_api_id int,
+player_name varchar(255),
+player_fifa_api_id int,
+birthday date,
+height double,
+weight smallint, 
+primary key(player_api_id));
+
 
 drop table if exists matches;
 
@@ -56,48 +96,139 @@ match_date  Date,
 match_api_id integer,
 home_team_api_id integer,
 away_team_api_id integer,
-home_team_goal integer,
-away_team_goal integer,
-PRIMARY KEY(match_id));
+home_team_goal tinyint,
+away_team_goal tinyint,
+home_player_1 integer,
+home_player_2 int,
+home_player_3 int,
+home_player_4 int,
+home_player_5 int,
+home_player_6 int,
+home_player_7 int,
+home_player_8 int,
+home_player_9 int,
+home_player_10 int,
+home_player_11 int,
+away_player_1 int,
+away_player_2 int,
+away_player_3 int,
+away_player_4 int,
+away_player_5 int,
+away_player_6 int,
+away_player_7 int ,
+away_player_8 int,
+away_player_9 int,
+away_player_10 int,
+away_player_11 int,
+PRIMARY KEY(match_id),
+key country_id (country_id),
+key league_id (league_id),
+key home_team_api_id (home_team_api_id),
+key away_team_api_id (away_team_goal),
+CONSTRAINT matches_ibfk_1 FOREIGN KEY (country_id) REFERENCES country (country_id),
+CONSTRAINT matches_ibfk_2 FOREIGN KEY (league_id) REFERENCES league (league_id),
+CONSTRAINT matches_ibfk_3 FOREIGN KEY (home_team_api_id) REFERENCES teams (team_api_id),
+CONSTRAINT matches_ibfk_4 FOREIGN KEY (away_team_api_id) REFERENCES teams (team_api_id),
+Constraint matches_ibfk_5 FOREIGN KEY (home_player_1 ) REFERENCES players (player_api_id),
+Constraint matches_ibfk_6 FOREIGN KEY (home_player_2 ) REFERENCES players (player_api_id),
+Constraint matches_ibfk_7 FOREIGN KEY (home_player_3 ) REFERENCES players (player_api_id),
+Constraint matches_ibfk_8 FOREIGN KEY (home_player_4 ) REFERENCES players (player_api_id),
+Constraint matches_ibfk_9 FOREIGN KEY (home_player_5 ) REFERENCES players (player_api_id),
+Constraint matches_ibfk_10 FOREIGN KEY (home_player_6 ) REFERENCES players (player_api_id),
+Constraint matches_ibfk_11 FOREIGN KEY (home_player_7 ) REFERENCES players (player_api_id),
+Constraint matches_ibfk_12 FOREIGN KEY (home_player_8 ) REFERENCES players (player_api_id),
+Constraint matches_ibfk_13 FOREIGN KEY (home_player_9 ) REFERENCES players (player_api_id),
+Constraint matches_ibfk_14 FOREIGN KEY (home_player_10 ) REFERENCES players (player_api_id),
+Constraint matches_ibfk_15 FOREIGN KEY (home_player_11 ) REFERENCES players (player_api_id),
+Constraint matches_ibfk_16 FOREIGN KEY (away_player_1 ) REFERENCES players (player_api_id),
+Constraint matches_ibfk_17 FOREIGN KEY (away_player_2 ) REFERENCES players (player_api_id),
+Constraint matches_ibfk_18 FOREIGN KEY (away_player_3 ) REFERENCES players (player_api_id),
+Constraint matches_ibfk_19 FOREIGN KEY (away_player_4 ) REFERENCES players (player_api_id),
+Constraint matches_ibfk_20 FOREIGN KEY (away_player_5 ) REFERENCES players (player_api_id),
+Constraint matches_ibfk_21 FOREIGN KEY (away_player_6 ) REFERENCES players (player_api_id),
+Constraint matches_ibfk_22 FOREIGN KEY (away_player_7 ) REFERENCES players (player_api_id),
+Constraint matches_ibfk_23 FOREIGN KEY (away_player_8 ) REFERENCES players (player_api_id),
+Constraint matches_ibfk_24 FOREIGN KEY (away_player_9 ) REFERENCES players (player_api_id),
+Constraint matches_ibfk_25 FOREIGN KEY (away_player_10 ) REFERENCES players (player_api_id),
+Constraint matches_ibfk_26 FOREIGN KEY (away_player_11 ) REFERENCES players (player_api_id));
 
-LOAD DATA INFILE 'c:/ProgramData/MySQL/MySQL Server 8.0/Uploads/match.csv' 
+
+
+
+LOAD DATA INFILE 'c:/ProgramData/MySQL/MySQL Server 8.0/Uploads/matches.csv' 
 INTO TABLE matches
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n' 
 ignore 1 rows
-;
-
-drop table if exists teams;
-
-CREATE TABLE teams 
 (
-team_id INTEGER NOT NULL,
-team_api_id integer,
-team_fifa_api_id  integer,
-team_long_name varchar (255), 
-team_short_name varchar(3), 
-PRIMARY KEY(team_id));
-
-LOAD DATA INFILE 'c:/ProgramData/MySQL/MySQL Server 8.0/Uploads/team.csv' 
-INTO TABLE teams
-CHARACTER SET CP1250
-FIELDS TERMINATED BY ',' 
-ENCLOSED BY '"'
-LINES TERMINATED BY '\r\n' 
-ignore 1 rows
-(team_id, team_api_id, @team_fifa_api_id, team_long_name, team_short_name)
-SET
-team_fifa_api_id = nullif(@team_fifa_api_id, 'NA')
+match_id,
+country_id ,
+league_id ,
+season,
+stage ,
+match_date , 
+match_api_id ,
+home_team_api_id ,
+away_team_api_id ,
+home_team_goal ,
+away_team_goal ,
+@home_player_1 ,
+@home_player_2 ,
+@home_player_3 ,
+@home_player_4 ,
+@home_player_5 ,
+@home_player_6 ,
+@home_player_7 ,
+@home_player_8 ,
+@home_player_9 ,
+@home_player_10 ,
+@home_player_11 ,
+@away_player_1 ,
+@away_player_2 ,
+@away_player_3 ,
+@away_player_4 ,
+@away_player_5 ,
+@away_player_6 ,
+@away_player_7 ,
+@away_player_8 ,
+@away_player_9 ,
+@away_player_10 ,
+@away_player_11 
+)
+SET 
+home_player_1  = nullif(@home_player_1 , ''),
+home_player_2  = nullif(@home_player_2 , ''),
+home_player_3  = nullif(@home_player_3 , ''),
+home_player_4  = nullif(@home_player_4 , ''),
+home_player_5  = nullif(@home_player_5 , ''),
+home_player_6  = nullif(@home_player_6 , ''),
+home_player_7  = nullif(@home_player_7 , ''),
+home_player_8  = nullif(@home_player_8 , ''),
+home_player_9  = nullif(@home_player_9 , ''),
+home_player_10  = nullif(@home_player_10 , ''),
+home_player_11  = nullif(@home_player_11 , ''),
+away_player_1  = nullif(@away_player_1 , ''),
+away_player_2  = nullif(@away_player_2 , ''),
+away_player_3  = nullif(@away_player_3 , ''),
+away_player_4  = nullif(@away_player_4 , ''),
+away_player_5  = nullif(@away_player_5 , ''),
+away_player_6  = nullif(@away_player_6 , ''),
+away_player_7  = nullif(@away_player_7 , ''),
+away_player_8  = nullif(@away_player_8 , ''),
+away_player_9  = nullif(@away_player_9 , ''),
+away_player_10  = nullif(@away_player_10 , ''),
+away_player_11  = nullif(@away_player_11 , '')
 ;
+
 
 
 drop table if exists team_attributes;
 
 CREATE TABLE team_attributes (
-team_id INTEGER NOT NULL,
+id INTEGER NOT NULL,
 team_fifa_api_id INT,
-team_api_id INT,
+team_api_id INT not null,
 team_date DATE,
 buildUpPlaySpeed tinyint,
 buildUpPlaySpeedClass VARCHAR(20),
@@ -120,7 +251,9 @@ defenceAggressionClass VARCHAR(20),
 defenceTeamWidth tinyint,
 defenceTeamWidthClass VARCHAR(20),
 defenceDefenderLineClass VARCHAR(20),
-PRIMARY KEY(team_id));
+PRIMARY KEY(id),
+key team_api_id (team_api_id),
+CONSTRAINT team_attributes_ibfk_1 foreign KEY (team_api_id) REFERENCES teams (team_api_id));
 
 LOAD DATA INFILE 'c:/ProgramData/MySQL/MySQL Server 8.0/Uploads/team_attributes.csv' 
 INTO TABLE team_attributes
@@ -130,7 +263,7 @@ ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n' 
 ignore 1 rows
 (
-team_id,
+id,
 team_fifa_api_id,
 team_api_id,
 team_date,
@@ -159,18 +292,6 @@ defenceDefenderLineClass
 SET
 buildUpPlayDribbling = nullif(@buildUpPlayDribbling, 'NA')
 ;
-
-DROP TABLE IF EXISTS players;
-
-CREATE TABLE players (
-player_id int not null,
-player_api_id int,
-player_name varchar(255),
-player_fifa_api_id int,
-birthday date,
-height double,
-weight smallint, primary key(player_id));
-
 
 LOAD DATA INFILE 'c:/ProgramData/MySQL/MySQL Server 8.0/Uploads/player.csv' 
 INTO TABLE players
@@ -226,7 +347,9 @@ gk_handling tinyint,
 gk_kicking tinyint,
 gk_positioning tinyint,
 gk_reflexes tinyint,
-primary key (player_attribute_id));
+primary key (player_attribute_id),
+key player_api_id (player_api_id),
+CONSTRAINT player_attributes_ibfk_1 foreign KEY (player_api_id) REFERENCES players (player_api_id));
 
 LOAD DATA INFILE 'c:/ProgramData/MySQL/MySQL Server 8.0/Uploads/player_attributes.csv' 
 INTO TABLE player_attributes
